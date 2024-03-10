626d26bfc128854575d3ed25570df3c6044be915
Lehan111 <149118561+Lehan111@users.noreply.github.com>
2024-03-10 16:05:48 -0400

    Create Introduction.py

---
+++ multi-page/Introduction.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.write("# COVID-19 dashboard (Italy)")
+st.write("The COVID-19 data comes from Center for Systems Science and Engineering (CSSE) at Johns Hopkins University, you could find the raw data [here]('https://github.com/CSSEGISandData/COVID-19').")
+st.write("By using this dashboard, you can view the frequency of daily cases, recoveries and deaths data in Italy. Also, it provides the distribution of transmission by dates. The surveilance system started from Januarary 2020, and ended up at October 2022.")
+st.write("*The dashboard was constructed by <span style='color:yellow;'>Lehan Xiao, Yawen Zhou and Hao Yan</span>.*", unsafe_allow_html=True)
